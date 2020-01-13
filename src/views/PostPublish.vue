ab30aca2117779721dc0fe0eb852550f05541334
jason <jason_xxx7@163.com>
2020-01-13 15:34:39 +0800

    8.3富文本框组件与视频组件添加

--- src/views/PostPublish.vue
+++ src/views/PostPublish.vue
@@ -14,6 +14,7 @@
         <el-form-item label="标题:">
           <el-input v-model="post.title"></el-input>
         </el-form-item>
+
         <!--单选框，选择发布是文章还是视频-->
         <el-form-item label="类型:">
           <el-radio-group v-model="post.type">
@@ -21,13 +22,32 @@
             <el-radio :label="2">视频</el-radio>
           </el-radio-group>
         </el-form-item>
+
+        <!-- 长传富文本和视频 -->
+        <el-form-item label="内容:">
+          <!-- 添加富文本框组件 需要添加必要的配置在export default的config: 中-->
+          <VueEditor :config="config" v-if="post.type===1" />
+          <!--添加视频组件-->
+          <el-upload class="upload-demo" action v-if="post.type===2">
+            <el-button size="small" type="primary">点击上传</el-button>
+            <div slot="tip" class="el-upload__tip">只能上传视频文件</div>
+          </el-upload>
+        </el-form-item>
+
       </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
+//通过npm i vue-word-editor --save下载富文本框，并引入
+import VueEditor from "vue-word-editor";
+import "quill/dist/quill.snow.css";
 export default {
+  //注册VueEditor，否则富文本无法使用
+  components: {
+    VueEditor
+  },
   data() {
     return {
       //根据api文档获取以下数据
@@ -37,6 +57,26 @@ export default {
         categories: [],
         cover: [],
         type: 1
+      },
+      config: {
+        // 上传图片的配置
+        uploadImage: {
+          url: "http://localhost:3000/upload",
+          name: "file",
+          // res是结果，insert方法会把内容注入到编辑器中，res.data.url是资源地址
+          uploadSuccess(res, insert) {
+            insert("http://localhost:3000" + res.data.url);
+          }
+        },
+
+        // 上传视频的配置
+        uploadVideo: {
+          url: "http://localhost:3000/upload",
+          name: "file",
+          uploadSuccess(res, insert) {
+            insert("http://localhost:3000" + res.data.url);
+          }
+        }
       }
     };
   }
